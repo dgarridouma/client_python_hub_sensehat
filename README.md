37c732f2151aa54746575f176d004aef0a69daae
dgm <dgm@uma.es>
2023-03-04 21:56:27 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# console_iothub
+Python client for Azure IoT Hub. It simulates measurements from a sensor including temperature, humidity and pressure.
+
+
---
+++ sensehat_iothub.py
@@ -0,0 +1,73 @@
+__author__ = 'dgarrido'
+
+DEVICE_ID='mi-dispositivo'
+REGISTRY_ID='mi-registro'
+IOTHUB_DEVICE_CONNECTION_STRING='HostName=miregistro.azure-devices.net;DeviceId=midispositivo;SharedAccessKey=y5mL1Fxxu/z7J9y1htJCc+1AgiAmOtqZM8kd6tP1BXs='
+
+import datetime
+import random
+import time
+import json
+from azure.iot.device import IoTHubDeviceClient, Message
+
+from sense_emu import SenseHat
+from threading import Lock
+
+sense = SenseHat() 
+period = 10
+blue = (0, 0, 255)
+yellow = (255, 255, 0)
+lock = Lock()
+
+
+# define behavior for receiving a message
+#{
+#  "period": 1,
+#  "message": "period changed"
+#}
+def message_handler(message):
+    global period
+    dict_command=json.loads(message.data)
+    period = int(dict_command['period'])
+    print(dict_command['message'])
+    lock.acquire()
+    sense.show_message(dict_command['message'], text_colour=yellow, back_colour=blue, scroll_speed=0.05)
+    lock.release()
+
+
+
+def main():
+    # The connection string for a device should never be stored in code. For the sake of simplicity we're using an environment variable here.
+    # The client object is used to interact with your Azure IoT hub.
+    device_client = IoTHubDeviceClient.create_from_connection_string(IOTHUB_DEVICE_CONNECTION_STRING)
+
+    # Connect the client.
+    device_client.connect()
+
+    # set the message handler on the client
+    device_client.on_message_received = message_handler
+
+    i=0
+    while True:
+        data=dict()
+        data['temperature']=random.randint(25,30)
+        data['humidity']=random.randint(50,100)
+        data['pressure']=random.randint(900,1100)
+        data['when']=datetime.datetime.now()
+        json_data=json.dumps(data,default=str)
+
+        message = Message(json_data)
+        message.content_encoding = "utf-8"
+        message.content_type = "application/json"
+
+        lock.acquire()
+        sense.show_message("{:.2f}".format(data['temperature'])+' '+"{:.2f}".format(data['humidity'])+' '+"{:.2f}".format(data['pressure']), text_colour=yellow, back_colour=blue, scroll_speed=0.05)
+        lock.release()
+
+        print(str(data['temperature'])+' '+str(data['humidity'])+' '+str(data['pressure']))
+
+        device_client.send_message(message)
+        time.sleep(period)
+
+if __name__ == '__main__':
+    main()
